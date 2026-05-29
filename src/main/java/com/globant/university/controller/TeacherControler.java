c06ba5fb7e071607e8f228a32b6e81e4d84428e9
Mars0606 <mars3507921@gmail.com>
2026-05-29 10:54:44 -0500

    capa Controller

---
+++ src/main/java/com/globant/university/controller/TeacherControler.java
@@ -0,0 +1,23 @@
+package com.globant.university.controller;
+
+import com.globant.university.model.Teacher;
+import com.globant.university.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/teacher")
+public class TeacherControler {
+
+    private final TeacherRepository teacherRepository;
+
+    @GetMapping("/getAll")
+    public List<Teacher> getAll(){
+        return teacherRepository.findAll();
+    }
+}
