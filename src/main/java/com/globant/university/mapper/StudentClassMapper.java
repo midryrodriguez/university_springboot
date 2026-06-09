918afdf91a3a208530d17105356b052b081f8b98
midryrodriguez <midry.rodriguez@globant.com>
2026-06-09 15:41:07 -0500

    List en Aclass Mapper

--- src/main/java/com/globant/university/mapper/StudentClassMapper.java
+++ src/main/java/com/globant/university/mapper/StudentClassMapper.java
@@ -3,6 +3,8 @@ package com.globant.university.mapper;
 import com.globant.university.dto.StudentClassResponseDTO;
 import com.globant.university.model.StudentClass;
 
+import java.util.List;
+
 public class StudentClassMapper {
 
     public static StudentClassResponseDTO modelToResponseDTO(StudentClass studentClass) {
@@ -12,5 +14,10 @@ public class StudentClassMapper {
                 studentClass.getAclass().getClassId(),
                 studentClass.getEnrolledAt()
         );
+    }
+
+
+    public static List<StudentClassResponseDTO> listModelToListResponseDTO(List<StudentClass> studentClassList) {
+        return studentClassList.stream().map(StudentClassMapper::modelToResponseDTO).toList();
     }
 }
